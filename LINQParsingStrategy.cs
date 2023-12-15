97aae5eee7b04814d55fc1c3202a1dab07f40502
zoyapanchenko <153904983+zoyapanchenko@users.noreply.github.com>
2023-12-15 11:28:03 +0200

    Add files via upload

---
+++ LINQParsingStrategy.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+
+namespace Lab2
+{
+    class LINQParsingStrategy : IXmlParserStrategy
+    {
+        List<Actor> info = new List<Actor>();
+        XDocument doc = new XDocument();
+
+        public List<Actor> Analyze(Actor actor, string path)
+        {
+            doc = XDocument.Load(path);
+            List<XElement> match = (from val in doc.Descendants("actor")
+                                    let screentime = int.Parse(val.Attribute("screentime").Value)
+                                    where
+                                    (
+                                        (actor.Director == null || actor.Director == val.Parent.Parent.Parent.Attribute("name").Value) &&
+                                        (actor.Year == null || actor.Year == val.Parent.Parent.Attribute("name").Value) &&
+                                        (actor.Movie == null || actor.Movie == val.Parent.Attribute("name").Value) &&
+                                        (actor.Name == null || actor.Name == val.Attribute("name").Value) &&
+                                        (actor.Screentime == null || actor.Screentime == val.Attribute("screentime").Value) &&
+                                        (actor.MinScreentime == -1 || screentime >= actor.MinScreentime) &&
+                                        (actor.MaxScreentime == 201 || screentime <= actor.MaxScreentime)
+                                    )
+                                    select val).ToList();
+
+            foreach (XElement obj in match)
+            {
+                Actor act = new Actor();
+                act.Director = obj.Parent.Parent.Parent.Attribute("name").Value;
+                act.Year = obj.Parent.Parent.Attribute("name").Value;
+                act.Movie = obj.Parent.Attribute("name").Value;
+                act.Name = obj.Attribute("name").Value;
+                act.Screentime = obj.Attribute("screentime").Value;
+                info.Add(act);
+            }
+
+            return info;
+        }
+    }
+}
---
+++ SAXParsingStrategy.cs
@@ -0,0 +1,121 @@
+﻿using System.Xml;
+
+namespace Lab2
+{
+    class SAXParsingStrategy : IXmlParserStrategy
+    {
+        private List<Actor> lastResult = null;
+
+        public List<Actor> Analyze(Actor actor, string path)
+        {
+            XmlReader reader = XmlReader.Create(path);
+            List<Actor> result = new List<Actor>();
+            Actor act = null;
+
+            string _director = null;
+            string _year = null;
+            string _movie = null;
+
+            while (reader.Read())
+            {
+                if (reader.Name == "director")
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        if (reader.Name == "name")
+                        {
+                            _director = reader.Value;
+                        }
+                    }
+                }
+                if (reader.Name == "year")
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        if (reader.Name == "name")
+                        {
+                            _year = reader.Value;
+                        }
+                    }
+                }
+                if (reader.Name == "movie")
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        if (reader.Name == "name")
+                        {
+                            _movie = reader.Value;
+                        }
+                    }
+                }
+                if (reader.Name == "actor")
+                {
+                    if (act == null)
+                    {
+                        act = new Actor();
+                        act.Director = _director;
+                        act.Year = _year;
+                        act.Movie = _movie;
+                    }
+                    else
+                    {
+                        act = new Actor();
+                        act.Director = _director;
+                        act.Year = _year;
+                        act.Movie = _movie;
+                    }
+
+                    if (reader.HasAttributes)
+                    {
+                        while (reader.MoveToNextAttribute())
+                        {
+                            if (reader.Name == "name")
+                            {
+                                act.Name = reader.Value;
+                            }
+                            if (reader.Name == "screentime")
+                            {
+                                act.Screentime = reader.Value;
+                            }
+                        }
+                    }
+                    result.Add(act);
+                }
+            }
+
+            lastResult = Filter(result, actor);
+            return lastResult;
+        }
+
+        public List<Actor> Filter(List<Actor> allActors, Actor param)
+        {
+            List<Actor> result = new List<Actor>();
+
+            if (allActors != null)
+            {
+                foreach (Actor e in allActors)
+                {
+                    try
+                    {
+                        int screentime = int.Parse(e.Screentime);
+
+                        if (
+                            (e.Director == param.Director || param.Director == null) &&
+                            (e.Year == param.Year || param.Year == null) &&
+                            (e.Movie == param.Movie || param.Movie == null) &&
+                            (e.Name == param.Name || param.Name == null) &&
+                            (e.Screentime == param.Screentime || param.Screentime == null) &&
+                            (param.MinScreentime == -1 || screentime >= param.MinScreentime) &&
+                            (param.MaxScreentime == 201 || screentime <= param.MaxScreentime)
+                            )
+                        {
+                            result.Add(e);
+                        }
+                    }
+                    catch { }
+                }
+            }
+            return result;
+        }
+    }
+}
